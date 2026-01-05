b86f18297983dd33faca932caf35ddf456a91e5b
Hermenegildo Hopson de Almeida da Cruz <138516502+HermenegildoDaCruz@users.noreply.github.com>
2026-01-05 21:40:08 +0100

    Creating nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 export default function Nav(){
     return (
-        <nav className="main-nav">
-            <ul>
-                <li>
-
+        <nav className="main-nav container">
+            <ul className="main-nav-list">
+                <li className="nav-list-item">
+                    <button>
+                        <ion-icon name="bookmark-outline" className = "icon"></ion-icon>
+                    <p>guardados</p>
+                        </button>
                 </li>
-                <li>
-                    
+                <li className="nav-list-item menu-btn">
+                    <button><ion-icon name="reorder-two-outline" className = "icon"></ion-icon></button>
                 </li>
             </ul>
         </nav>
