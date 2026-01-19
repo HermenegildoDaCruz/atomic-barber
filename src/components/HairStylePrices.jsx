13029cad792bfe27f0c26416843f2962e81e5012
Hermenegildo Hopson de Almeida da Cruz <138516502+HermenegildoDaCruz@users.noreply.github.com>
2026-01-19 16:32:48 +0100

    updating HairStylePrices.jsx

--- src/components/HairStylePrices.jsx
+++ src/components/HairStylePrices.jsx
@@ -1,32 +1,47 @@
-import { useState } from "react";
 import { hairstylePrices } from "../data/hairstyle-prices";
 import AnimatedListItem from "./AnimatedListItem";
 import { useBooleanHook } from "../hooks/useBooleanHook";
 
-export default function HairStylePrices(){
-  const { value: showAdultsPrices, setTrue: showAdultsHairstyles, setFalse: showChildrenHairstyles } = useBooleanHook(true);
-  const hairstyles = showAdultsPrices ? hairstylePrices.adults:hairstylePrices.children
+export default function HairStylePrices() {
+  const {
+    value: showAdultsPrices,
+    setTrue: showAdultsHairstyles,
+    setFalse: showChildrenHairstyles,
+  } = useBooleanHook(true);
+  const hairstyles = showAdultsPrices
+    ? hairstylePrices.adults
+    : hairstylePrices.children;
 
   return (
     <section className="hairstyle-prices container">
       <h2 className="heading-secondary">Cortes de cabelo</h2>
       <menu className="menu">
         <li>
-          <button className={showAdultsPrices ? "menu-btn btn-actived":"menu-btn"} onClick={showAdultsHairstyles}>Adultos</button>
+          <button
+            className={showAdultsPrices ? "menu-btn btn-actived" : "menu-btn"}
+            onClick={showAdultsHairstyles}
+          >
+            Adultos
+          </button>
         </li>
         <li>
-          <button className={!showAdultsPrices ? "menu-btn btn-actived":"menu-btn"} onClick={showChildrenHairstyles}>Crianças</button>
+          <button
+            className={!showAdultsPrices ? "menu-btn btn-actived" : "menu-btn"}
+            onClick={showChildrenHairstyles}
+          >
+            Crianças
+          </button>
         </li>
       </menu>
       <ul className="hairstyle-list">
         {hairstyles.map((hairstyle) => (
           <AnimatedListItem key={hairstyle.id} className="hairstyle">
             <div className="hairstyle-img-box">
-                <img
-              src={hairstyle.image}
-              alt={hairstyle.name}
-              className="hairstyle-img"
-            />
+              <img
+                src={hairstyle.image}
+                alt={hairstyle.name}
+                className="hairstyle-img"
+              />
             </div>
             <h3 className="heading-terciary">{hairstyle.name}</h3>
             <p className="hairstyle-price">{hairstyle.price} A0A</p>
