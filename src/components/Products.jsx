f88934d4bfd33e4671a39cc05943952b3474fd89
Hermenegildo Hopson de Almeida da Cruz <138516502+HermenegildoDaCruz@users.noreply.github.com>
2026-01-28 15:36:42 +0100

    Updating media-queries.css

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,14 @@
 import { products } from "../data/products";
 import ProductItem from "./ProductItem";
 export default function Products() {
-  return (
-    <section className="products-section container" id="products">
-      <h2 className="heading-secondary">Nossos Produtos</h2>
-      <ul className="products-list grid grid-4">
-        {products.map((product) => (
-          <ProductItem key={product.id} item={product} />
-        ))}
-      </ul>
-    </section>
-  );
+    return (
+        <section className="products-section container" id="#product">
+            <h2 className="heading-secondary">Nossos Produtos</h2>
+            <ul className="products-list grid grid-4">
+                {products.map(product => (
+                    <ProductItem key={product.id} item={product} />
+                ))}
+            </ul>
+        </section>
+    );
 }
